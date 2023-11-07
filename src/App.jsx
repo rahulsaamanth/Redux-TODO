984d87504f38b13a58b4b744628c2c7dfcaca675
rahulsaamanth <rahulsaamanth@yahoo.com>
2023-11-08 00:05:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { AddTodo } from "./components/AddTodo"
+import Todos from "./components/Todos"
+
+function App() {
+  return (
+    <>
+      <div className="div">Redux</div>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
